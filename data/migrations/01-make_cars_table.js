321931090f9830db2422516e85e634748d397847
Christian Anderson <tehninjawaffle@gmail.com>
2023-12-20 07:44:23 -0800

    Filled out the basic migration for migrating up

--- data/migrations/01-make_cars_table.js
+++ data/migrations/01-make_cars_table.js
@@ -1,5 +1,14 @@
 exports.up = function (knex) {
   // DO YOUR MAGIC
+  return knex.schema.createTable("cars", table => {
+    table.increments("id")
+    table.string("vin", 17).notNullable().unique()
+    table.string("make", 100).notNullable()
+    table.string("model", 100).notNullable()
+    table.numeric("mileage", 999999).notNullable()
+    table.string("title", 100)
+    table.string("transmission", 100)
+  })
 };
 
 exports.down = function (knex) {
