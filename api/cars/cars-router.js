65d91100cbc64a1d14886ccdc37f8712f7638b40
Christian Anderson <tehninjawaffle@gmail.com>
2023-12-21 12:44:33 -0800

    Added the first getAll for router as well as some error handling

--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,1 +1,27 @@
 // DO YOUR MAGIC
+
+const router = require("express").Router()
+const Car = require("./cars-model")
+
+const {checkCarId, checkCarPayload, checkVinNumberUnique, checkVinNumberValid} = require("./cars-middleware")
+
+
+router.get("/", (req, res, next) => {
+  Car.getAll()
+  .then(cars => {
+    res.status(200).json(cars)
+  })
+  .else(error => {
+    next(error)
+  })
+})
+
+
+router.use((error, req, res, next) => { // eslint-disable-line
+  res.status(error.status || 500).json({
+    message: error.message,
+    customMessage: "Something bad inside of the cars-router"
+  })
+})
+
+module.exports = router;
