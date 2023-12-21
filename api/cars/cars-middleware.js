a1085b8abc0ead4920050466f54329266226af2a
Christian Anderson <tehninjawaffle@gmail.com>
2023-12-21 08:17:49 -0800

    imported db and Car and exported all my middelware, fleshed out what I think will be a usable checkCarId

--- api/cars/cars-middleware.js
+++ api/cars/cars-middleware.js
@@ -1,5 +1,15 @@
+const db = require("../../data/db-config")
+const Car = require("./cars-model")
+
 const checkCarId = (req, res, next) => {
   // DO YOUR MAGIC
+  const id = req.params.id;
+  const car = Car.getById(id)
+  if (!car) {
+    res.status(404).json({
+      message: `car with id ${id} is not found`
+    })
+  }
 }
 
 const checkCarPayload = (req, res, next) => {
@@ -13,3 +23,5 @@ const checkVinNumberValid = (req, res, next) => {
 const checkVinNumberUnique = (req, res, next) => {
   // DO YOUR MAGIC
 }
+
+module.exports = { checkCarId, checkCarPayload, checkVinNumberValid, checkVinNumberUnique }
